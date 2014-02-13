fd3ba8b69bf15515c7ec615c003279021399fbcd
Pol Van Aubel <pol@karpenoktem.nl>
2014-02-13 22:15:40 +0100

    More robust writing of DMX, better error messages.

--- src/dmxdriver.c
+++ src/dmxdriver.c
@@ -15,11 +15,11 @@ purge_buffers(struct ftdi_context *ftdic) {
 	int ret;
 	ret = ftdi_usb_purge_buffers(ftdic);
 	if (ret == -1) {
-		fprintf(stderr, "Read buffer purge failed");
+		fprintf(stderr, "purge_buffers: Read buffer purge failed");
 	} else if (ret == -2) {
-		fprintf(stderr, "Write buffer purge failed");
+		fprintf(stderr, "purge_buffers: Write buffer purge failed");
 	} else if (ret == -3) {
-		fprintf(stderr, "USB device unavailable");
+		fprintf(stderr, "purge_buffers: USB device unavailable");
 	}
 	return ret;
 }
@@ -30,25 +30,39 @@ purge_receive_buffer(struct ftdi_context *ftdic) {
 	int ret;
 	ret = ftdi_usb_purge_rx_buffer(ftdic);
 	if (ret == -1) {
-		fprintf(stderr, "Read buffer purge failed");
+		fprintf(stderr, "purge_receive_buffer: Read buffer purge failed");
 	} else if (ret == -2) {
-		fprintf(stderr, "USB device unavailable");
+		fprintf(stderr, "purge_receive_buffer: USB device unavailable");
 	}
 	return ret;
 }
 
 
-static void
-send_msg_error(int ret) {
-	if (ret == -666) {
-		fprintf(stderr, "USB Device Unavailable for ftdi_write_data\n");
-	} else if (ret < 0) {
-		fprintf(stderr, "ftdi_write_data returned error %d from usb_bulk_write\n", ret);
-	} else {
-		fprintf(stderr, "Unexpected number of bytes written: %d\n", ret);
-	}
-}
+/*
+ * Function that writes exactly length bytes to the ftdi device.
+ * Buffer must be at least length bytes in length.
+ */
+static int
+write_data(struct ftdi_context *ftdic, unsigned char *buffer, int length) {
+	int ret;
+	int bytes_written = 0;
 
+	while (bytes_written < length) {
+		ret = ftdi_write_data(ftdic, buffer + bytes_written, length - bytes_written);
+		if (ret < 0) {
+			if (ret == -666) {
+				fprintf(stderr, "write_data: USB Device Unavailable for ftdi_write_data\n");
+			} else if (ret < 0) {
+				fprintf(stderr, "write_data: ftdi_write_data returned error %d from usb_bulk_write\n", ret);
+			}
+			return -1;
+		}
+		bytes_written += ret;
+	}
+
+	assert(bytes_written == length);
+	return bytes_written;
+}
 
 static int
 send_msg(struct ftdi_context *ftdic, int label, unsigned char *data, int length) {
@@ -63,23 +77,23 @@ send_msg(struct ftdi_context *ftdic, int label, unsigned char *data, int length)
 	header[3] = length >> BYTE_LENGTH;
 
 	// Write The Header
-	ret = ftdi_write_data(ftdic, header, MSG_HEADER_LENGTH);
+	ret = write_data(ftdic, header, MSG_HEADER_LENGTH);
 	if (ret != MSG_HEADER_LENGTH) {
-		send_msg_error(ret);
+		fprintf(stderr, "send_msg: Unexpected number of bytes written: %d\n", ret);
 		return -1;
 	}
 
 	// Write The Data
-	ret = ftdi_write_data(ftdic, data, length);
+	ret = write_data(ftdic, data, length);
 	if (ret != length) {
-		send_msg_error(ret);
+		fprintf(stderr, "send_msg: Unexpected number of bytes written: %d\n", ret);
 		return -1;
 	}
 
 	// Write End Code
-	ret = ftdi_write_data(ftdic, &end_code, 1);
+	ret = write_data(ftdic, &end_code, 1);
 	if (ret != 1) {
-		send_msg_error(ret);
+		fprintf(stderr, "send_msg: Unexpected number of bytes written: %d\n", ret);
 		return -1;
 	}
 
@@ -87,7 +101,7 @@ send_msg(struct ftdi_context *ftdic, int label, unsigned char *data, int length)
 }
 
 
-void
+int
 send_dmx(struct mk2_pro_context *mk2c, unsigned char *dmxbytes) {
 	unsigned char my_dmx[513];
 	int ret;
@@ -101,20 +115,21 @@ send_dmx(struct mk2_pro_context *mk2c, unsigned char *dmxbytes) {
 	ret = send_msg(mk2c->ftdic, SEND_DMX_2, my_dmx, 513);
 	if (ret < 0)
 	{
-		fprintf(stderr, "FAILED to send DMX ... exiting. FIXME THIS MESSAGE TO BE USEFUL!\n");
-		// TODO fix "nice" shutdown of FTDI.
+		fprintf(stderr, "send_dmx: Failed to send DMX\n");
+		return -1;
 	}
+	return 0;
 }
 
 
 static void
-receive_msg_error(int ret) {
+read_data_error(int ret) {
 	if (ret == -666) {
-		fprintf(stderr, "USB Device Unavailable for ftdi_read_data\n");
+		fprintf(stderr, "read_data: USB Device Unavailable for ftdi_read_data\n");
 	} else if (ret < 0) {
-		fprintf(stderr, "ftdi_read_data returned error %d from libusb_bulk_transfer\n", ret);
+		fprintf(stderr, "read_data: ftdi_read_data returned error %d from libusb_bulk_transfer\n", ret);
 	} else {
-		fprintf(stderr, "Unexpected number of bytes read: %d\n", ret);
+		fprintf(stderr, "read_data: Unexpected number of bytes read: %d\n", ret);
 	}
 }
 
@@ -134,7 +149,7 @@ read_data(struct mk2_pro_context *mk2c, unsigned char *buffer, int length) {
 		//fprintf(stderr, "Reading data, still %d to read.", length - bytes_read);
 		ret = ftdi_read_data(mk2c->ftdic, buffer + bytes_read, length - bytes_read);
 		if (ret < 0) {
-			receive_msg_error(ret);
+			read_data_error(ret);
 			return -1;
 		}
 		bytes_read += ret;
@@ -160,6 +175,7 @@ receive_msg(struct mk2_pro_context *mk2c, struct application_message *appmsg) {
 	//fprintf(stderr, "Reading new packet header.\n");
 	ret = read_data(mk2c, header, MSG_HEADER_LENGTH);
 	if (ret == -1) {
+		fprintf(stderr, "receive_msg: Unable to read message header\n");
 		return -1;
 	}
 
@@ -172,12 +188,12 @@ receive_msg(struct mk2_pro_context *mk2c, struct application_message *appmsg) {
 		length += ((int) header[3]) << BYTE_LENGTH;
 
 		if ((header[0] != MSG_START_CODE) || (length > 600)) {
-			fprintf(stderr, "Received:\nStart code: 0x%X\nLabel:      %u\nLength:     %u\n", header[0], label, length);
-			fprintf(stderr, "Header invalid. Cycling...\n");
+			fprintf(stderr, "receive_msg: Received start code 0x%X, label %u, length %u\n", header[0], label, length);
+			fprintf(stderr, "receive_msg: Header invalid. Cycling...\n");
 			
 			/* Move the header one byte, discarding the "wrong" MSG_START_CODE, then read another byte and repeat */
 			memmove(header, header + 1, MSG_HEADER_LENGTH - 1);
-			ret = read_data(mk2c, header + 3, 1);
+			ret = read_data(mk2c, header + MSG_HEADER_LENGTH - 1, 1);
 			if (ret != 1) {
 				return -1;
 			}
@@ -189,17 +205,19 @@ receive_msg(struct mk2_pro_context *mk2c, struct application_message *appmsg) {
 	
 	ret = read_data(mk2c, buffer, length);
 	if (ret == -1) {
+		fprintf(stderr, "receive_msg: Unable to read message data\n");
 		return -1;
 	}
 
 	/* Check end code */
 	ret = read_data(mk2c, header, 1);
 	if (ret == -1) {
+		fprintf(stderr, "receive_msg: Unable to read message end code\n");
 		return -1;
 	}
 
 	if (header[0] != MSG_END_CODE) {
-		fprintf(stderr, "End code not at expected location.\n");
+		fprintf(stderr, "receive_msg: End code not at expected location\n");
 		return -1;
 	}
 
@@ -220,9 +238,10 @@ read_dmx_usb_mk2_pro(struct mk2_pro_context *mk2c) {
 		ret = receive_msg(mk2c, &appmsg);
 
 		if (ret < 0) {
-			fprintf(stderr, "Error occurred during receive. Purging receive buffer and retrying.\n");
+			fprintf(stderr, "read_dmx_usb_mk2_pro: Error occurred during receive. Purging receive buffer and retrying.\n");
 			ret = purge_receive_buffer(mk2c->ftdic);
 			if (ret != 0) {
+				fprintf(stderr, "read_dmx_usb_mk2_pro: Error purging receive buffer. Signalling error and killing thread.\n");
 				mk2c->running = 0;
 				mk2c->error_callback(-1);
 				pthread_exit(NULL);
@@ -232,25 +251,25 @@ read_dmx_usb_mk2_pro(struct mk2_pro_context *mk2c) {
 
 		/* TODO handle everything that is not DMX message. */
 		if (appmsg.label != 5) {
-			fprintf(stderr, "Unable to handle message with label %d.\n", appmsg.label);
+			fprintf(stderr, "read_dmx_usb_mk2_pro: Unable to handle message with label %d.\n", appmsg.label);
 			continue;
 		}
 
 		if ((appmsg.data[0] & 0x02) != 0) {
-			fprintf(stderr, "Widget receive overrun occurred. DMX Data invalid.\n");
+			fprintf(stderr, "read_dmx_usb_mk2_pro: Widget receive overrun occurred. DMX Data invalid.\n");
 			continue;
 		} else if ((appmsg.data[0] & 0x01) != 0) {
-			fprintf(stderr, "Widget receive queue overflowed. DMX Data invalid.\n");
+			fprintf(stderr, "read_dmx_usb_mk2_pro: Widget receive queue overflowed. DMX Data invalid.\n");
 			continue;
 		}
 
 		if (appmsg.data[1] != 0) {
-			fprintf(stderr, "Received DMX start code not equal to 0x00: 0x%X\n", appmsg.data[1]);
+			fprintf(stderr, "read_dmx_usb_mk2_pro: WARNING: Received DMX start code not equal to 0x00: 0x%X\n", appmsg.data[1]);
 		}
 
 		for (int i = 0; i < 512; ++i) {
 			if (appmsg.data[i + 2] != dmx_state[i]) {
-				fprintf(stderr, "Channel %d changed to value %d\n", i + 1, appmsg.data[i + 2]);
+				fprintf(stderr, "read_dmx_usb_mk2_pro: Channel %d changed to value %d\n", i + 1, appmsg.data[i + 2]);
 				/* todo callback */
 				mk2c->update_callback(i, dmx_state[i], appmsg.data[i+2]);
 				dmx_state[i] = appmsg.data[i+2];
@@ -276,31 +295,31 @@ connect_dmx_usb_mk2_pro(struct ftdi_context *ftdic) {
 	ret = ftdi_usb_open_desc(ftdic, 0x0403, 0x6001, "DMX USB PRO Mk2", NULL);
 	switch (ret) {
 		case -3:
-			fprintf(stderr, "USB device not found\n");
+			fprintf(stderr, "connect_dmx_usb_mk2_pro: USB device not found\n");
 			break;
 		case -4:
-			fprintf(stderr, "Unable to open device\n");
+			fprintf(stderr, "connect_dmx_usb_mk2_pro: Unable to open device\n");
 			break;
 		case -5:
-			fprintf(stderr, "Unable to claim device\n");
+			fprintf(stderr, "connect_dmx_usb_mk2_pro: Unable to claim device\n");
 			break;
 		case -6:
-			fprintf(stderr, "Device reset failed\n");
+			fprintf(stderr, "connect_dmx_usb_mk2_pro: Device reset failed\n");
 			break;
 		case -7:
-			fprintf(stderr, "Setting baud rate failed\n");
+			fprintf(stderr, "connect_dmx_usb_mk2_pro: Setting baud rate failed\n");
 			break;
 		case -8:
-			fprintf(stderr, "Getting product description failed\n");
+			fprintf(stderr, "connect_dmx_usb_mk2_pro: Getting product description failed\n");
 			break;
 		case -9:
-			fprintf(stderr, "Getting serial number failed\n");
+			fprintf(stderr, "connect_dmx_usb_mk2_pro: Getting serial number failed\n");
 			break;
 		case -12:
-			fprintf(stderr, "libusb_get_device_list() failed\n");
+			fprintf(stderr, "connect_dmx_usb_mk2_pro: libusb_get_device_list() failed\n");
 			break;
 		case -13:
-			fprintf(stderr, "libusb_get_device_descriptor() failed\n");
+			fprintf(stderr, "connect_dmx_usb_mk2_pro: libusb_get_device_descriptor() failed\n");
 			break;
 	}
 	return ret;
@@ -314,13 +333,13 @@ enable_second_universe(struct ftdi_context *ftdic) {
 
 	ret = send_msg(ftdic, SET_API_KEY, APIKey, 4);
 	if (ret != 0) {
-		fprintf(stderr, "Setting API key failed.");
+		fprintf(stderr, "enable_second_universe: Setting API key failed.");
 		return ret;
 	}
 
 	ret = send_msg(ftdic, SET_PORT_ASSIGNMENT, port_set, 2);
 	if (ret != 0) {
-		fprintf(stderr, "Setting port assignment failed.");
+		fprintf(stderr, "enable_second_universe: Setting port assignment failed.");
 		return ret;
 	}
 
@@ -337,7 +356,7 @@ set_dmx_recv_mode(struct ftdi_context *ftdic, unsigned char on_change) {
 
 	ret = send_msg(ftdic, RECEIVE_DMX_ON_CHANGE_1, &on_change, 1);
 	if (ret != 0) {
-		fprintf(stderr, "Setting receive mode failed.");
+		fprintf(stderr, "set_dmx_recv_mode: Setting receive mode failed.");
 	}
 
 	return ret;
@@ -351,33 +370,37 @@ init_dmx_usb_mk2_pro(dmx_update_callback_t update_callback, dmx_commit_callback_
 
 	mk2c = malloc(sizeof(struct mk2_pro_context));
 	if (mk2c == NULL) {
-		fprintf(stderr, "Error allocating memory for mk2_pro_context.");
-		exit(1);
+		fprintf(stderr, "init_dmx_usb_mk2_pro: Error allocating memory for mk2_pro_context.");
+		return NULL;
 	}
 
 	mk2c->ftdic = ftdi_new();
 	if (mk2c->ftdic == NULL) {
 		free(mk2c);
-		fprintf(stderr, "ftdi_init failed\n");
+		fprintf(stderr, "init_dmx_usb_mk2_pro: ftdi_init failed\n");
 		return NULL;
 	}
 
 	ret = connect_dmx_usb_mk2_pro(mk2c->ftdic);
 	if (ret != 0) {
+		fprintf(stderr, "init_dmx_usb_mk2_pro: Failed to connect\n");
 		goto error;
 	}
 
 	ret = purge_buffers(mk2c->ftdic);
 	if (ret != 0) {
+		fprintf(stderr, "init_dmx_usb_mk2_pro: Failed to purge buffers\n");
 		goto error;
 	}
 
 	ret = enable_second_universe(mk2c->ftdic);
 	if(ret != 0) {
+		fprintf(stderr, "init_dmx_usb_mk2_pro: Failed to enable second universe\n");
 		goto error;
 	}
 	ret = set_dmx_recv_mode(mk2c->ftdic, 0);
 	if(ret != 0) {
+		fprintf(stderr, "init_dmx_usb_mk2_pro: Failed to set dmx receive mode\n");
 		goto error;
 	}
 
@@ -385,7 +408,11 @@ init_dmx_usb_mk2_pro(dmx_update_callback_t update_callback, dmx_commit_callback_
 	mk2c->commit_callback = commit_callback;
 	mk2c->error_callback = error_callback;
 	mk2c->running = 1;
-	pthread_create(&mk2c->readid, NULL, read_dmx_usb_mk2_pro_runner, mk2c);
+	ret = pthread_create(&mk2c->readid, NULL, read_dmx_usb_mk2_pro_runner, mk2c);
+	if (ret != 0) {
+		fprintf(stderr, "init_dmx_usb_mk2_pro: %s\n", strerror(ret));
+		goto error;
+	}
 
 	return mk2c;
 
@@ -398,8 +425,12 @@ error:
 
 void
 teardown_dmx_usb_mk2_pro(struct mk2_pro_context *mk2c) {
+	int ret;
 	mk2c->running = 0;
-	pthread_join(mk2c->readid, NULL);
+	ret = pthread_join(mk2c->readid, NULL);
+	if (ret != 0) {
+		fprintf(stderr, "teardown_dmx_usb_mk2_pro: %s\n", strerror(ret));
+	}
 	purge_buffers(mk2c->ftdic);
 	ftdi_usb_close(mk2c->ftdic);
 	ftdi_free(mk2c->ftdic);
