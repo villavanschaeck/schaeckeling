ce4f97fd8ff9a4cab86a85a1a9e6702562606781
Jille Timmermans <jille@karpenoktem.nl>
2014-03-25 13:28:44 +0100

    generate-programma will now write csv-files

--- scripts/generate-programma.php
+++ scripts/generate-programma.php
@@ -2,27 +2,35 @@
 	// <config>
 	$lamps = 4;
 	$intensity = array(
-		1,
-		1,
-		1,
-		0.2,
+		'100%',
+		'100%',
+		'100%',
+		'20%',
 	);
 	$channelsPerLamp = 8;
 	$c = array(
-		'off' => array(0, 0, 0),
-		'white' => array(220, 200, 100),
-		'red' => array(255, 0, 0),
-		'orange' => array(255, 127, 0),
-		'yellow' => array(255, 255, 0),
-		'green' => array(0, 255, 0),
-		'blue' => array(0, 0, 255),
-		'cyan' => array(0, 255, 255),
-		'purple' => array(200, 0, 255),
-		'magenta' => array(255, 255, 0),
+		'black',
+		'red',
+		'orange',
+		'yellow',
+		'green',
+		'turqoise',
+		'blue',
+		'lila',
+		'pink',
+		'magenta',
 	);
 	// </config>
 
 	assert($channelsPerLamp >= 3);
+
+	$header = array();
+	for($lamp = 0; $lamp < $lamps; $lamp++) {
+		$header[] = 'Kleur '. ($lamp+1);
+		$header[] = 'Intensiteit '. ($lamp+1);
+	}
+	echo "!author;generate-programma.php\n";
+	echo implode(';', $header) ."\n";
 
 	$modes = array(
 		'all',
@@ -37,19 +45,14 @@
 
 	$prog = array();
 	for($step = 0; $step < $steps; $step++) {
-		$state = array_fill(0, $channels, '  0');
+		$stepColors = array_fill(0, $lamps, '');
+		$stepIntensities = $intensity;
 
 		$mode = $modes[array_rand($modes)];
 		switch($mode) {
 			case 'all':
 				$color = $c[array_rand($c)];
-				for($lamp = 0; $lamp < $lamps; $lamp++) {
-					for($i = 0; $i < 3; $i++) {
-						$v = round($color[$i] * $intensity[$lamp]);
-						$v = sprintf('% 3d', $v);
-						$state[$lamp * $channelsPerLamp + $i] = $v;
-					}
-				}
+				$stepColors = array_fill(0, $lamps, $color);
 				break;
 			case 'grouped':
 				$l = range(0, $lamps - 1);
@@ -57,30 +60,22 @@
 				foreach(array_chunk($l, round(sqrt($lamps))) as $group) {
 					$color = $c[array_rand($c)];
 					foreach($group as $lamp) {
-						for($i = 0; $i < 3; $i++) {
-							$v = round($color[$i] * $intensity[$lamp]);
-							$v = sprintf('% 3d', $v);
-							$state[$lamp * $channelsPerLamp + $i] = $v;
-						}
+						$stepColors[$lamp] = $color;
 					}
 				}
 				break;
 			case 'random':
 				for($lamp = 0; $lamp < $lamps; $lamp++) {
 					$color = $c[array_rand($c)];
-					for($i = 0; $i < 3; $i++) {
-						$v = round($color[$i] * $intensity[$lamp]);
-						$v = sprintf('% 3d', $v);
-						$state[$lamp * $channelsPerLamp + $i] = $v;
-					}
+					$stepColors[$lamp] = $color;
 				}
 				break;
 		}
-		$prog[] = '{'. implode(', ', $state) .'}';
+		$line = array_fill(0, $lamps * 2, '');
+		for($lamp = 0; $lamp < $lamps; $lamp++) {
+			$line[$lamp * 2] = $stepColors[$lamp];
+			$line[$lamp * 2 + 1] = $stepIntensities[$lamp];
+		}
+		echo implode(';', $line) ."\n";
 	}
 ?>
-int programma_steps = <?php echo $steps; ?>;
-int programma_channels = <?php echo $channels; ?>;
-unsigned char programma[<?php echo $steps; ?>][<?php echo $channels; ?>] = {
-	<?php echo implode(",\n\t", $prog) ."\n"; ?>
-};
